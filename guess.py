6a07dc2f3dcb838b049789ffa64442c55e7ae0e4
leroxe <mrmoon123322@gmail.com>
2025-10-30 18:39:46 +0500

    optimize

--- guess.py
+++ guess.py
@@ -1,8 +1,12 @@
 from random import randint
-g = randint(1,100)
+
+g = randint(1, 100)
 while True:
-    n = int(input('Введи число: '))
-    if g>n: print("меньше")
-    elif g<n: print("больше")
-    elif g==n: break
+    n = int(input("Введи число: "))
+    if g > n:
+        print("меньше")
+    elif g < n:
+        print("больше")
+    elif g == n:
+        break
 print("пабеда")
