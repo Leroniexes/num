c125fa7dd077d564b90ff42b1e669c7b044419de
leroxe <mrmoon123322@gmail.com>
2025-10-30 18:14:54 +0500

    guess_number

--- guess.py
+++ guess.py
@@ -3,6 +3,6 @@ g = randint(1,100)
 while True:
     n = int(input('Введи число: '))
     if g>n: print("меньше")
-    if g<n: print("больше")
-    if g==n: break
+    elif g<n: print("больше")
+    elif g==n: break
 print("пабеда")
